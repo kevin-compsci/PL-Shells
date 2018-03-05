bec86ed2915ccdba69b9b051bb568ef784b31eb4
Kevin Nguyen <25444103+kevin-compsci@users.noreply.github.com>
2018-03-04 19:59:35 -0600

    Add files via upload

---
+++ PL-Shells/C/C-Shell.c
@@ -0,0 +1,23 @@
+//Kevin Nguyen
+//Basic Shell for C++ (Cpp).
+
+/*
+How to run (linux):
+	1.) open command line and cd into directory folder of where this code is
+	2.) type in "gcc *.c" to compile c files
+	3.) type in "a.out" to run out file
+*/
+
+//include statements
+#include <stdio.h>
+
+//function definitions
+
+//main driver
+int main() {
+	//local declarations
+  	printf("Hello, World!\n");
+  	return 0;
+}
+
+//Functions implementations goes below
